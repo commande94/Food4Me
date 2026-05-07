037f754e13b94c7cb7cf89893a58dceb7e6e1cf7
miguel <miguel@gmail.com>
2026-05-07 23:08:32 +0100

    Fin de commit

--- Food4Me/food4me-api/routes/repasRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const repasController = require("../controllers/repasController");
-
-router.post("/ajouter", repasController.ajouterRepas);
-router.post("/ajouter-complet", repasController.ajouterRepasComplet);
-
-module.exports = router;
